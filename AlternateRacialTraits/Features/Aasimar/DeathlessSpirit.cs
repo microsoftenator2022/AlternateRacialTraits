8a95c87de2de033117a4e6785439bb9c5116f374
Aaron Maslen <aaron.maslen@gmail.com>
2024-07-05 13:25:29 +1000

    Fix Deathless Spirit

--- AlternateRacialTraits/Features/Aasimar/DeathlessSpirit.cs
+++ AlternateRacialTraits/Features/Aasimar/DeathlessSpirit.cs
@@ -27,6 +27,11 @@ using MicroWrath.Deferred;
 using MicroWrath.Localization;
 
 using static MicroWrath.Encyclopedia;
+using Kingmaker.EntitySystem.Persistence.Versioning;
+using Newtonsoft.Json.Linq;
+using Kingmaker.UnitLogic;
+using MicroWrath.Util;
+using static UnityEngine.Rendering.DebugUI;
 
 namespace AlternateRacialTraits.Features.Aasimar;
 
@@ -88,10 +93,8 @@ internal static partial class DeathlessSpirit
 
     internal static IDeferredBlueprint<BlueprintFeature> Create()
     {
-        var guid = new BlueprintGuid(Guid.NewGuid());
-
         return
-            Deferred.NewBlueprint<BlueprintFeature>(guid, nameof(DeathlessSpirit))
+            Deferred.NewBlueprint<BlueprintFeature>(GeneratedGuid.Get(nameof(DeathlessSpirit)))
                 .Map(blueprint =>
                 {
                     blueprint.m_DisplayName = Localized.DisplayName;
@@ -127,6 +130,52 @@ internal static partial class DeathlessSpirit
 
                     return blueprint;
                 })
-                .AddBlueprintDeferred(guid);
+                .AddBlueprintDeferred(GeneratedGuid.DeathlessSpirit);
+    }
+
+    internal class SaveFixer : JsonUpgraderBase
+    {
+        public override bool NeedsPlayerPriorityLoad => false;
+
+        public override bool WillUpgrade(string jsonName) => true;
+
+        public override void Upgrade()
+        {
+            MicroLogger.Debug(() => $"{nameof(DeathlessSpirit)}.{nameof(SaveFixer)}");
+
+            var valuesToFix = new List<JValue>();
+
+            foreach (var fact in base.Root.SelectTokens("..m_Facts").OfType<JArray>().SelectMany(Functional.Identity).OfType<JObject>())
+            {
+                if (fact["Components"] is not JObject components || !components.Properties().Any(p => p.Name.StartsWith("$DeathlessSpirit")))
+                    continue;
+
+                var blueprintGuidString = fact["Blueprint"].ToString();
+
+                if (BlueprintGuid.Parse(blueprintGuidString) == GeneratedGuid.DeathlessSpirit.Guid)
+                    continue;
+
+                MicroLogger.Warning("Found broken Deathless Spirit feature. Fixing");
+
+                foreach (var value in base.Root.SelectTokens("$..*").OfType<JValue>().Where(v => v.Type == JTokenType.String && v.ToString() == blueprintGuidString))
+                {
+                    MicroLogger.Debug(() => $"Found matching guid at {value.Path}");
+
+                    valuesToFix.Add(value);
+
+                }
+            }
+
+            foreach (var p in valuesToFix)
+            {
+                p.Value = GeneratedGuid.DeathlessSpirit.Guid.ToString();
+            }
+        }
+
+        const int SaveFixerId = -231;
+
+        [Init]
+        static void Init() => 
+            JsonUpgradeSystem.Register(SaveFixerId, "Alternate Racial Traits: Fix Deathless Spirit guid", new SaveFixer());
     }
 }
